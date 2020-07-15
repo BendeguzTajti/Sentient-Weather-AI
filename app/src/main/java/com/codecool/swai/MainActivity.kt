c50ebea9b090e9d42ece1bad2a475ce05f9f6164
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-07-15 16:51:19 +0200

    add bottomsheet listener

--- app/src/main/java/com/codecool/swai/MainActivity.kt
+++ app/src/main/java/com/codecool/swai/MainActivity.kt
@@ -3,11 +3,44 @@ package com.codecool.swai
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.widget.NestedScrollView
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var bottomSheet: BottomSheetBehavior<NestedScrollView>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        bottomSheet = BottomSheetBehavior.from(detailsPage)
+        addBottomSheetListener()
+    }
+
+    private fun addBottomSheetListener() {
+        bottomSheet.addBottomSheetCallback(object: BottomSheetBehavior.BottomSheetCallback() {
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                if (mainPage.isAnimating) {
+                    mainPage.pauseAnimation()
+                }
+                swipeIndicator.visibility = View.INVISIBLE
+            }
+
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                if (mainPage.progress > 0.0f) {
+                    mainPage.resumeAnimation()
+                }
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    swipeIndicator.setAnimation(R.raw.swipe_down)
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    swipeIndicator.setAnimation(R.raw.swipe_up)
+                }
+                swipeIndicator.playAnimation()
+                swipeIndicator.visibility = View.VISIBLE
+            }
+
+        })
     }
 }
