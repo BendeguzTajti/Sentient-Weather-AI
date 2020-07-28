dc3ac9ce6e69abc1e6e953a85a4b47ac6a3ac207
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-07-28 21:29:27 +0200

    add functions to access modified data

--- app/src/main/java/com/codecool/swai/model/WeatherCurrent.kt
+++ app/src/main/java/com/codecool/swai/model/WeatherCurrent.kt
@@ -1,7 +1,51 @@
 package com.codecool.swai.model
 
+import com.codecool.swai.R
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.TimeUnit
+import kotlin.math.roundToInt
+
 class WeatherCurrent {
-    data class Result(val weather: List<Weather>, val main: Temperature, val timezone: Long, val name: String, val cod: Int)
-    data class Weather(val main: String, val description: String)
-    data class Temperature(val temp: Double)
+
+    data class Result(val weather: List<Weather>, val main: Temperature, val timezone: Long, val name: String, val cod: Int) {
+
+        fun getCurrentHour(): Int {
+            val timeZone = TimeZone.getTimeZone("UTC")
+            timeZone.rawOffset = TimeUnit.SECONDS.toMillis(timezone).toInt()
+            val dateFormat = SimpleDateFormat("HH", Locale.getDefault())
+            dateFormat.timeZone = timeZone
+            return dateFormat.format(Date()).toInt()
+        }
+
+        fun getLocationName(): String {
+            return name.replace(" keruelet", " kerület", false)
+        }
+    }
+
+    data class Weather(val main: String, val description: String) {
+
+        fun getWeatherIcon(currentHour: Int): Int {
+            return when(main) {
+                "Thunderstorm" -> R.raw.thunder
+                "Drizzle" -> R.raw.drizzle
+                "Rain" -> if (currentHour in 6..17) R.raw.rainy_day else R.raw.rainy_night
+                "Snow" -> if (currentHour in 6..17) R.raw.snow_day else R.raw.snow_night
+                "Clear" -> if (currentHour in 6..17) R.raw.clear_day else R.raw.clear_night
+                "Clouds" -> if (currentHour in 6..17) R.raw.cloudy_day else R.raw.cloudy_night
+                else -> R.raw.atmosphere
+            }
+        }
+    }
+
+    data class Temperature(val temp: Double) {
+
+        fun getTempCelsius(): String {
+            return (temp - 273.15).roundToInt().toString() + "°C"
+        }
+
+        fun getTempFahrenheit(): String {
+            return (((temp - 273.15) * 9 / 5) + 32).roundToInt().toString() + "°F"
+        }
+    }
 }
--- app/src/main/java/com/codecool/swai/model/WeatherForecast.kt
+++ app/src/main/java/com/codecool/swai/model/WeatherForecast.kt
@@ -1,9 +1,63 @@
 package com.codecool.swai.model
 
+import com.codecool.swai.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
 class WeatherForecast {
-    data class Result(val cod: Int, val list: List<Forecast>, val city: CityName)
-    data class Forecast(val main: Temperature, val weather: List<Weather>, val dt_txt: String)
-    data class Temperature(val temp: Double)
-    data class Weather(val main: String, val description: String)
-    data class CityName(val name: String)
+    data class Result(val daily: List<Forecast>)
+    data class Forecast(val dt: Long, val temp: Temperature, val weather: List<Weather>) {
+
+        @ExperimentalStdlibApi
+        fun getDay(): String {
+            val sdf = SimpleDateFormat("EEEE", Locale.getDefault())
+            val date = Date(dt * 1000)
+            return sdf.format(date).capitalize(Locale.getDefault())
+        }
+
+    }
+    data class Temperature(val min: Double, val max: Double) {
+
+        fun getMinMaxTempCelsius(): String {
+            val maxCelsius = (max - 273.15).roundToInt().toString() + "°"
+            val minCelsius = (min - 273.15).roundToInt().toString() + "°"
+            return "$maxCelsius / $minCelsius"
+        }
+
+        fun getMinMaxTempFahrenheit(): String {
+            val maxFahrenheit = (((max - 273.15) * 9 / 5) + 32).roundToInt().toString() + "°"
+            val minFahrenheit = (((min - 273.15) * 9 / 5) + 32).roundToInt().toString() + "°"
+            return "$maxFahrenheit / $minFahrenheit"
+        }
+
+    }
+    data class Weather(val main: String) {
+
+        fun getWeatherIcon(): Int {
+            return when(main) {
+                "Thunderstorm" -> R.raw.thunder
+                "Drizzle" -> R.raw.drizzle
+                "Rain" -> R.raw.rainy_day
+                "Snow" -> R.raw.snow_day
+                "Clear" -> R.raw.clear_day
+                "Clouds" -> R.raw.cloudy_day
+                else -> R.raw.atmosphere
+            }
+        }
+
+        fun getWeatherType(): Int {
+            return when(main) {
+                "Thunderstorm" -> R.string.type_thunder
+                "Drizzle" -> R.string.type_drizzle
+                "Rain" -> R.string.type_rain
+                "Snow" -> R.string.type_snow
+                "Clear" -> R.string.type_clear
+                "Clouds" -> R.string.type_clouds
+                else -> R.string.type_mist
+            }
+        }
+
+    }
+
 }
