c560bf0931f40c1b88441554a748ca7af844c73f
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-07-28 21:23:51 +0200

    add new url to fetch forecast data

--- app/src/main/java/com/codecool/swai/api/WeatherApiService.kt
+++ app/src/main/java/com/codecool/swai/api/WeatherApiService.kt
@@ -12,33 +12,22 @@ import retrofit2.http.Query
 interface WeatherApiService {
 
     @GET("weather")
-    fun getCurrentWeatherByCity(@Query("q") q: String,
-                                @Query("units") units: String,
-                                @Query("APPID") APPID: String,
-                                @Query("lang") lang: String): Single<WeatherCurrent.Result>
+    fun getCurrentWeather(@Query("lat") lat: Double,
+                          @Query("lon") lon: Double,
+                          @Query("APPID") APPID: String,
+                          @Query("lang") lang: String): Single<WeatherCurrent.Result>
 
-    @GET("forecast")
-    fun getWeatherForecastByCity(@Query("q") q: String,
-                                 @Query("units") units: String,
-                                 @Query("APPID") APPID: String,
-                                 @Query("lang") lang: String): Single<WeatherForecast.Result>
-
-    @GET("weather")
-    fun getCurrentWeatherByCoordinates(@Query("lat") lat: Double,
-                                       @Query("lon") lon: Double,
-                                       @Query("units") units: String,
-                                       @Query("APPID") APPID: String,
-                                       @Query("lang") lang: String): Single<WeatherCurrent.Result>
-
-    @GET("forecast")
-    fun getWeatherForecastByCoordinates(@Query("lat") lat: Double,
-                                        @Query("lon") lon: Double,
-                                        @Query("units") units: String,
-                                        @Query("APPID") APPID: String,
-                                        @Query("lang") lang: String): Single<WeatherForecast.Result>
+    @GET("onecall")
+    fun getWeatherForecast(@Query("lat") lat: Double,
+                           @Query("lon") lon: Double,
+                           @Query("exclude") exclude: String,
+                           @Query("APPID") APPID: String,
+                           @Query("lang") lang: String): Single<WeatherForecast.Result>
 
     companion object {
+
         private const val BASE_URL = "http://api.openweathermap.org/data/2.5/"
+
         fun create(): WeatherApiService {
             val retrofit = Retrofit.Builder()
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
