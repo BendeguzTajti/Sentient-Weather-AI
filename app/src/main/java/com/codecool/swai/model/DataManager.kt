4ec009622aa9cf2f77b2fcffe6efbd3fb79fa6de
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-07-28 21:24:53 +0200

    impelement api with new endpoint

--- app/src/main/java/com/codecool/swai/model/DataManager.kt
+++ app/src/main/java/com/codecool/swai/model/DataManager.kt
@@ -14,17 +14,9 @@ class DataManager {
         WeatherApiService.create()
     }
 
-    fun getWeatherDataByCity(cityName: String): Single<Weather> {
-        val currentWeather = weatherApiService.getCurrentWeatherByCity(cityName, "metric", apiKey, Locale.getDefault().language)
-        val forecastWeather = weatherApiService.getWeatherForecastByCity(cityName, "metric", apiKey, Locale.getDefault().language)
-        return Single.zip(currentWeather, forecastWeather, BiFunction { weather, forecast ->
-            Weather(weather, forecast)
-        })
-    }
-
     fun getWeatherDataByCoordinates(latitude: Double, longitude: Double): Single<Weather> {
-        val currentWeather = weatherApiService.getCurrentWeatherByCoordinates(latitude, longitude, "metric", apiKey, Locale.getDefault().language)
-        val forecastWeather = weatherApiService.getWeatherForecastByCoordinates(latitude, longitude, "metric", apiKey, Locale.getDefault().language)
+        val currentWeather = weatherApiService.getCurrentWeather(latitude, longitude, apiKey, Locale.getDefault().language)
+        val forecastWeather = weatherApiService.getWeatherForecast(latitude, longitude, "current,minutely,hourly", apiKey, Locale.getDefault().language)
         return Single.zip(currentWeather, forecastWeather, BiFunction { weather, forecast ->
             Weather(weather, forecast)
         })
