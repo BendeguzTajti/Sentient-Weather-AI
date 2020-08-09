3594166b4f06b319d0f3efd6cae898ffa8125df1
BendeguzTajti <bendeguz9ctajti@gmail.com>
2020-08-09 19:50:48 +0200

    create methods related to information dialog

--- app/src/main/java/com/codecool/swai/contract/BaseContract.kt
+++ app/src/main/java/com/codecool/swai/contract/BaseContract.kt
@@ -1,12 +1,17 @@
 package com.codecool.swai.contract
 
+import android.view.LayoutInflater
+import androidx.appcompat.app.AlertDialog
+
 interface BaseContract {
 
     interface BaseView {
         fun displayError(exception: Throwable)
+        fun requestPermission(permission: String, requestCode: Int)
     }
 
     interface BasePresenter {
         fun onDetach()
+        fun buildPermissionDialog(inflater: LayoutInflater, message: String, permission: String, requestCode: Int): AlertDialog
     }
 }
